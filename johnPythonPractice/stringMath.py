3dc801e604c6e2444b54a56a191fd13053eb46a0
John Walter Greathouse <jgreatho@hydra12.eecs.utk.edu>
2020-03-08 13:20:24 -0400

    testing -John

---
+++ johnPythonPractice/stringMath.py
@@ -0,0 +1,14 @@
+# John Greathouse
+# This function is an attempt to farmiliarize myself with Python
+# 
+# What I'm aiming to do:
+#    Make a string-math based input function
+
+# This is probalby a really bad use of python, but it'll force me to learn how to write in this language!
+
+
+def main
+	
+	#code goes here
+
+
