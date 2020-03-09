31e51895f04f36b4c65238b01804ca7c1ad80a79
owencqueen <owencqueen@hotmail.com>
2020-03-09 11:39:44 -0400

    New functions in template file.

---
+++ template_class.py
@@ -0,0 +1,40 @@
+
+class rubiks_cube:
+	
+	def __init__(self):
+
+	f = []
+	b = []
+	u = []
+	d = []
+	l = []
+	r = []
+
+	def check_cube( precedence ):
+		# Does the brunt of the work
+		# Reorders the cubes to move where they need to 
+
+	def rotate(face, direction):
+		# Math of reordering the cube - alters face
+		# Calls check_cube(face)
+
+	# Types of rotations
+	#    Simple functions that just call rotate on given face
+	def front():
+	def front_prime():
+	def back():
+	def back_prime():
+	
+	def up():
+	def up_prime():
+	def down():
+	def down_prime():
+
+	def left():
+	def left_prime():
+	def right():
+	def right_prime():
+
+	# Other functions
+	def shuffle(rotations):
+	def if_solved():
