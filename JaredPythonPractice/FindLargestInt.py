0db1493fc12839fef05e4206923601f80c6d934b
Jared Garbe Staman <jstaman@hydra13.eecs.utk.edu>
2020-03-13 23:39:47 -0400

    mini-challenge 7 stuff

---
+++ JaredPythonPractice/FindLargestInt.py
@@ -0,0 +1,14 @@
+
+#simple program that reads in a list of numbers and then prints the largest
+
+
+a=[]
+
+n=int(input("Enter number of elements: "))
+
+for i in range(1,n+1):
+	b=int(input("Enter element: "))
+	a.append(b)
+
+a.sort()
+print("Largest element is:",a[n-1])
