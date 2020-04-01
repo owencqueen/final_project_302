b21570c8f69fddeb9d63d192dceddeee194070d7
owencqueen <oqueen@DESKTOP-3EDQN74.localdomain>
2020-03-31 22:43:37 -0400

    Made additions to check_cube

--- check_cube.py
+++ check_cube.py
@@ -28,19 +28,19 @@ def check_cube(self, precedence, direction):
 
 		if (direction == "regular"):
 			for i in range(0, self.dim):
-				temp_val = self.r[i][self.dim]
-				self.r[i][self.dim] = self.d[self_dim][i]
-				self.d[self_dim][i] = self.l[i][0]
-				self.l[i][0] = self.u[0][i]
+				temp_val = self.r[i][self.dim - 1]
+				self.r[i][self.dim - 1] = self.d[self_dim - i - 1][i]
+				self.d[self_dim - i - 1][i] = self.l[self.dim - i - 1][0]
+				self.l[self.dim - i - 1][0] = self.u[0][i]
 				self.u[0][i] = temp_val
 			
 		else:
 			for i in range(0, self.dim):
-				temp_val = self.r[i][self.dim]
-				self.r[i][self.dim] = self.u[0][i]
-				self.u[0][i] = self.l[i][0]
-				self.l[i][0] = self.d[self.dim][i]
-				self.d[self.dim][i] = temp_val
+				temp_val = self.r[i][self.dim - 1]
+				self.r[i][self.dim - 1] = self.u[0][i]
+				self.u[0][i] = self.l[self.dim  - i - 1][0]
+				self.l[self.dim - i - 1][0] = self.d[self.dim - i - 1][i]
+				self.d[self.dim - i - 1][i] = temp_val
 			
 				
 
@@ -52,30 +52,72 @@ def check_cube(self, precedence, direction):
 		#  3. Top of d
 		#  4. Right of l
 
-		if (direction == "regular":
+		if (direction == "regular"):
 			for i in range(0, self.dim):
 				temp_val = self.r[i][0]
-				self.r[i][0] = self.u[self.dim][i]
-				self.u[self.dim][i] = self.l[i][self.dim]
-				self.l[i][self.dim] = self.d[0][i]
-				self.d[0][i] = temp_val
+				self.r[i][0] = self.u[self.dim - 1][i]
+				self.u[self.dim - 1][i] = self.l[self - i - 1][self.dim]
+				self.l[self.dim - i - 1][self.dim - 1] = self.d[0][self - i - 1]
+				self.d[0][self - i - 1] = temp_val
 		else:
 			for i in range(0, self.dim):
 				temp_val = self.r[i][0]
-				self.r[i][0] = self.d[0][i]
-				self.d[0][i] = self.l[i][self.dim]
-				self.l[i][self.dim] = self.u[self.dim][i]
-				self.u[self_dim][i] = temp_val
+				self.r[i][0] = self.d[0][self.dim - i - 1]
+				self.d[0][self.dim - i - 1] = self.l[i][self.dim - i - 1]
+				self.l[i][self.dim - i - 1] = self.u[self.dim - 1][i]
+				self.u[self_dim - 1][i] = temp_val
 
 
 	elif (precedence == "l"):
 		R = 0
+
+		# To be rotated:
+		#  1. Left of u
+		#  2. Left of d
+		#  3. Left of f
+		#  4. Right of b
+
+		if (direction == "regular"):
+			for i in range(0, self.dim):
+				temp_val = self.u[i][0]
+				self.u[i][0] = self.b[i][self.dim - 1]
+				self.b[i][self.dim - 1] = self.d[self.dim - i - 1][0]
+				self.d[self.dim - i - 1][0] = self.f[i][0]
+				self.f[i][0] = temp_val
+
+		else:
+			for i in range(0, self.dim):
+				temp_val = self.u[i][0]
+				self.u[i][0] = self.f[i][0]
+				self.f[i][0] = self.d[self.dim - i - 1][0]
+				self.d[self.dim - i - 1][0] = self.b[i][self.dim - 1]
+				self.b[i][self.dim - 1] = temp_val
+
+	
 	elif (precedence == "r"):
 		L = 0
-		if (direction == "regular"):
 			
-		else:
+		# To be rotated:
+		#  1. Right of f
+		#  2. Right of u
+		#  3. Left of b
+		#  4. Right of d
 
+		if (direction == "regular"):			
+			for i in range(0, self.dim):
+				temp_val = self.u[self.dim - 1][i]
+				self.u[self.dim - 1][i] = self.f[self.dim - 1][i]
+				self.f[self.dim - 1][i] = self.d[self.dim - 1][i]
+				self.d[self.dim - 1][i] = self.b[0][self.dim - i - 1]
+				self.b[0][self.dim - i - 1] = temp_val
+
+		else:
+			for i in range(0, self.dim):
+				temp_val = self.u[self.dim - 1][i]
+				self.u[self.dim - 1][i] = self.b[0][self.dim - i - 1]
+				self.b[0][self.dim - i - 1] = self.d[self.dim - 1][i]
+				self.d[self.dim - 1][i] = self.f[self.dim - 1][i]
+				self.f[self.dim - 1][i] = temp_val
 
 
 	elif (precedence == "u"):
