546e07240447f1f3e4680ce49f175e2ec474377a
Jared Garbe Staman <jstaman@hydra13.eecs.utk.edu>
2020-04-20 20:18:48 -0400

    decent solver, could use work

--- tester.py
+++ tester.py
@@ -2,26 +2,75 @@ import keras
 from template_class import rubiks_cube
 from solver_helpers import flatten_faces
 from solver_helpers import two_dim_data
+from solver_helpers import get_move
+from solver_helpers import counter_move
+from solver_helpers import names_of_moves
+from solver_helpers import random_move
 
 def main():
 
 	#create rubiks_cube object
 	cube = rubiks_cube()
 
+	mod_name = input("What model do you want to use? ")
 
 	shuf_num = input("How many shuffles? ")
 	cube.shuffle(int(shuf_num))
 
-	#turning cube data into numpy array
-	flat = flatten_faces(cube)
-	data = two_dim_data(flat)
-	
-	#put cube into model
-	model = keras.models.load_model('models/jared_model')
 
-	#predict needs 4dimensions, not sure if this is right?
-	out = model.predict(data.reshape(1,2,12,1))
-	#don't know what squeeze does
-	out = out.squeeze()
+	model = keras.models.load_model("models/" + mod_name)
+
+	last_move = -1  #keeps track of last move done
+	repeat_same_move = 0  #keeps track of repeating the same move
+	count = 0 #keeps track of all moves done
+	
+	mvs = names_of_moves()  #used for conversion to letter for comparison
+
+	while(cube.if_solved() == 0) :
+
+		#model reads in data and makes prediction
+		flat = flatten_faces(cube)
+		data = two_dim_data(flat)
+		out = model.predict_classes(data.reshape(1,2,12,1))
+		
+		#if this move is the counter of the last move (model is repeating itself) or if model has repeated same move 4 times
+		if((mvs[last_move] == counter_move(mvs[out[0]]) and rand_check != 1 and last_move != -1) or (repeat_same_move == 4)):
+			#perform a random move on cube
+			random_move(cube)
+			print(out[0]),
+			print("called random move") 
+		
+			#rand_check makes sure we don't call random check multiple times in a row
+			rand_check = 1
+
+			#reset counter for repeated moves
+			repeat_same_move = 0
+		else:
+			#perform model's predicted move
+			print(out[0])
+			mv = get_move(cube, out[0])
+			mv()
+
+			#reset counter for repeated moves to 0 if the last move was different than current move
+			if(out[0] != last_move) :
+				repeat_same_move = 0
+			else:
+				repeat_same_move+=1
+	
+			#keep track of last move
+			last_move = out[0]
+
+			rand_check = 0
+	
+		#while loop ends if we have done 50 moves
+		count+=1
+		if(count == 50) :
+			break
+
+	
+
+	final_state = flatten_faces(cube)
+	print(final_state)
+
 
 main()
