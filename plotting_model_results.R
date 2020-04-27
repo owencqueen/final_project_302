2ee0fce506f6c98c6f7714b13ef4febade7c69ee
Owen Queen <owencqueen@hotmail.com>
2020-04-26 20:55:18 -0400

    Updated R script

--- plotting_model_results.R
+++ plotting_model_results.R
@@ -12,21 +12,29 @@ plot_solve_acc <- function(omt, omtr, nmt, nmtr){
     
   par(mfrow = c(2,2)) # Create 2x2 grid of plots
   
-  barplot( omt$solve_prop ~ omt$rotations, col = "steelblue", xlab = "Number of Rotations", ylab = "Proportion Solved", main = "oboorg1.model on model_test")
+  plot( omt$solve_prop ~ omt$rotations, col = "steelblue", xlab = "Number of Rotations", ylab = "Proportion Solved", main = "oboorg1.model on model_test")
   
-  barplot(omtr$solve_prop ~ omtr$rotations, col = "springgreen", xlab = "Number of Rotations", ylab = "Proportion Solved", main = "oboorg1.model on model_test_random")
+  plot(omtr$solve_prop ~ omtr$rotations, col = "springgreen", xlab = "Number of Rotations", ylab = "Proportion Solved", main = "oboorg1.model on model_test_random")
 
-  barplot(nmt$solve_prop ~ nmt$rotations, col = "plum", xlab = "Number of Rotations", ylab = "Proportion Solved", main = "obonew1.model on model_test")
+  plot(nmt$solve_prop ~ nmt$rotations, col = "plum", xlab = "Number of Rotations", ylab = "Proportion Solved", main = "obonew1.model on model_test")
   
-  barplot(nmtr$solve_prop ~ nmtr$rotations, col = "navy", xlab = "Number of Rotations", ylab = "Proportion Solved", main = "obonew1.model on model_test_random")
+  plot(nmtr$solve_prop ~ nmtr$rotations, col = "navy", xlab = "Number of Rotations", ylab = "Proportion Solved", main = "obonew1.model on model_test_random")
   
 }
 
 psa_together <- function(omt, omtr, nmt, nmtr){
-  tog = table(omt$solve_prop, omtr$solve_prop, nmt$solve_prop, nmtr$solve_prop)
+    
+  #par(mfrow = c(2,2)) # Create 2x2 grid of plots
+    
+  plot( omt$solve_prop ~ omt$rotations, type = 'l', col = "blue", xlab = "Number of Rotations", ylab = "Proportion Solved", main = "Models on Regular vs. Random Tester")
+    
+  lines(omtr$solve_prop, col = "darkgreen", xlab = "Number of Rotations", ylab = "Proportion Solved", main = "oboorg1.model on model_test_random")
+    
+  lines(nmt$solve_prop, col = "red", xlab = "Number of Rotations", ylab = "Proportion Solved", main = "obonew1.model on model_test")
+    
+  lines(nmtr$solve_prop, col = "pink", xlab = "Number of Rotations", ylab = "Proportion Solved", main = "obonew1.model on model_test_random")
   
-  barplot(tog, main = "Accuracies of ML models", xlab = "Number of Rotations", ylab = "Proportion Solved", beside = TRUE, col = c("green", "red", "navy", "purple"))
-  
+  legend("topright", cex = 0.8, title = "Legend",legend = c("org regular", "org random", "new regular", "new random"), col = c("blue", "darkgreen", "red", "pink"), lty = c(1,1,1,1),ncol = 1)
 }
 
 psa_together(omt = org_mt_data, omtr = org_mtr_data, nmt = new_mt_data, nmtr = new_mtr_data) 
